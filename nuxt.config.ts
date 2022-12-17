0a3c6b043adf9ef2f31bf620fb2fbe454e56c26d
motarou <kubotake1gomiru15@gmail.com>
2022-12-17 16:53:59 +0900

    add global scss and assets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,11 @@
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  css: ["vuetify/lib/styles/main.sass", "mdi/css/materialdesignicons.min.css"],
+  css: [
+    "vuetify/lib/styles/main.sass",
+    "mdi/css/materialdesignicons.min.css",
+    /* add my scss*/ "@/assets/style/main.scss",
+  ],
   build: {
     transpile: ["vuetify"],
   },
