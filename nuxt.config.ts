333102155c8dc2a47d865f620953e625fbe10a91
motarou <kubotake1gomiru15@gmail.com>
2022-12-20 17:47:36 +0900

    fix header and nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,7 @@ export default defineNuxtConfig({
           name: 'viewport',
           content: 'width=device-width, initial-scale=1.0',
         },
-        { name: 'description', content: '' },
+        { name: 'description', content: 'この中に説明が入るよ' },
         //{ name: 'robots', content: 'noindex,follow' }, ホーム以外のページのseo対策があまりできていないときに使う
 
         //ogp
