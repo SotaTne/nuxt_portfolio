4e1b80d04d3dc475b0176a8b0e6b9857e18cc0eb
motarou <kubotake1gomiru15@gmail.com>
2022-12-28 11:38:49 +0900

    add test contact form

--- components/Contact_form.vue
+++ components/Contact_form.vue
@@ -21,25 +21,34 @@
     </div>
   </form>
   -->
-  <form name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field">
+  <form name="contact" method="POST" netlify>
+    <!-- ここに追加 -->
+    <form name="contact" method="POST" netlify>
+      <p>
+        <label>Your Name: <input type="text" name="name" /></label>
+      </p>
+      <p>
+        <label>Your Email: <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <label>Your Role: <select name="role[]" multiple>
+            <option value="leader">Leader</option>
+            <option value="follower">Follower</option>
+          </select></label>
+      </p>
+      <p>
+        <label>Message: <textarea name="message"></textarea></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+
+    <input type="hidden" name="form-name" value="contact" />
     <p>
       <label>Your Name: <input type="text" name="name" /></label>
     </p>
-    <p>
-      <label>Your Email: <input type="email" name="email" /></label>
-    </p>
-    <p>
-      <label>Your Role: <select name="role[]" multiple>
-          <option value="leader">Leader</option>
-          <option value="follower">Follower</option>
-        </select></label>
-    </p>
-    <p>
-      <label>Message: <textarea name="message"></textarea></label>
-    </p>
-    <p>
-      <button type="submit">Send</button>
-    </p>
+    <!-- 省略します -->
   </form>
 </template>
 <style lang="scss">
